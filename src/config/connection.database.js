c615f36d09c81756daa1c8855dc37e4160cdb354
Phat Cong <phamcongphat.ptpmcn@gmail.com>
2024-03-05 20:49:53 +0700

    fix bug ,create logs/access

--- src/config/connection.database.js
+++ src/config/connection.database.js
@@ -5,13 +5,12 @@ const getConnection = () => {
     host: "127.0.0.1",
     port: 3306,
     user: "root",
-<<<<<<< HEAD
+
     password: "password",
     database: "clean_house",
-=======
     password: "123456",
     database: "clean_house_services",
->>>>>>> fd6edef47b61e2e216a192d55f629827e15d51dd
+
   });
 };
 
