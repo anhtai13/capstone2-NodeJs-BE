241a77c9f11c9a645b2690599c2dbe3f1e0b1418
thanhdatyr <imneyu@gmail.com>
2024-03-29 22:07:53 +0700

    API Login Employee

--- src/application/services/auth.service.js
+++ src/application/services/auth.service.js
@@ -1,26 +1,39 @@
-import authRepositories from "../repositories/auth.repositories.js"
+import authRepositories from "../repositories/auth.repositories.js";
+import bcrypt from "bcryptjs";
 
 const login = (params, callback) => {
-    authRepositories.login(params, (err, result) => {
-        if (err) {
-            callback(err, null);
-        } else {
-            callback(null, result);
-        }
-    })
-}
+  authRepositories.login(params, (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, result);
+    }
+  });
+};
 
 const logout = (params, callback) => {
-    authRepositories.logout(params, (err, result) => {
-        if (err) {
-            callback(err, null);
-        } else {
-            callback(null, result);
-        }
-    })
-}
+  authRepositories.logout(params, (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, result);
+    }
+  });
+};
+
+const login1 = (params, callback) => {
+  authRepositories.login1(params, (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, result);
+    }
+  });
+};
 
 export default {
-    login,
-    logout
-}
+  login,
+  logout,
+
+  login1,
+};
