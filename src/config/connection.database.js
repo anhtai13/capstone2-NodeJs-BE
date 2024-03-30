70cb631281c792525f1c4df426ac577d8d9ff0a9
anhtai13 <tainguyen2322001@gmail.com>
2024-03-30 13:18:21 +0700

    commit

--- src/config/connection.database.js
+++ src/config/connection.database.js
@@ -1,23 +1,24 @@
-import mysql from "mysql2";
+import mysql from "mysql";
+
+const getConnection = () => {
+  return mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "clean_house",
+  });
+};
 
 // const getConnection = () => {
 //   return mysql.createConnection({
 //     host: "127.0.0.1",
 //     port: 3306,
 //     user: "root",
-//     password: "password",
-//     database: "clean_house",
+//     password: "123456",
+//     database: "clean_house_services",
 //   });
 // };
-const getConnection = () => {
-  return mysql.createConnection({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "123456",
-    database: "clean_house_services",
-  });
-};
 
 
 // const getConnection = () => {
