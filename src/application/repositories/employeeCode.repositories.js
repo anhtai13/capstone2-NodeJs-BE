a4d8a530bc085e4c3e19449dfad44a330d809e12
anhtai13 <tainguyen2322001@gmail.com>
2024-05-15 15:39:06 +0700

    updated: rename fil query

--- src/application/repositories/employeeCode.repositories.js
+++ src/application/repositories/employeeCode.repositories.js
@@ -46,14 +46,21 @@ const getListDebtHistory = (params, callback) => {
 // lấy danh sách các nhân viên có tổng tiền nợ
 const getListEmployeeReceipt = (params, callback) => {
   connection.query(
-    `SELECT u.*, ed.*,dh.*, SUM(od.unit_price) AS employee_debt,
-    format( (SUM(od.unit_price) - SUM(dh.price)),0) AS balance
-     FROM emoloyee_debt ed
-     JOIN order_details od ON ed.order_detail_id = od.order_detail_id
-     JOIN users u ON od.employee_code = u.user_id
-     LEFT JOIN debt_history dh ON ed.emoloyee_debt_id = dh.employee_debt_id
-     WHERE od.responeCode IS NULL AND u.role = 3
-     GROUP BY u.user_id;`,
+    `SELECT u.*, COALESCE(BANG_A.debt, 0) AS debt, COALESCE(BANG_B.receipt, 0) AS re, (COALESCE(BANG_A.debt, 0) - COALESCE(BANG_B.receipt, 0)) as balance
+    FROM users u
+    LEFT JOIN
+        (SELECT od.employee_code, SUM(od.unit_price) AS debt
+        FROM emoloyee_debt ed
+        JOIN order_details od ON ed.order_detail_id = od.order_detail_id
+        WHERE od.responeCode IS NULL
+        GROUP BY od.employee_code) AS BANG_A
+        ON u.user_id = BANG_A.employee_code
+    LEFT JOIN
+        (SELECT dh.user_id, SUM(dh.price) as receipt
+        FROM debt_history dh
+        GROUP BY dh.user_id) AS BANG_B
+        ON u.user_id = BANG_B.user_id
+    WHERE u.role = 3;`,
     [params],
     (error, results) => {
       if (error) {
@@ -68,18 +75,21 @@ const getListEmployeeReceipt = (params, callback) => {
 // lấy danh sách các nhân viên có tổng tiền nợ
 const getListEmployeeReceiptId = (params, callback) => {
   connection.query(
-    `SELECT BANG_A.*, BANG_B.sum_nhan, (BANG_A.sum_no - BANG_B.sum_nhan) as sum_can_tra FROM
-    (SELECT u.*, SUM(od.unit_price) AS sum_no
-    FROM emoloyee_debt ed
-    JOIN order_details od ON ed.order_detail_id = od.order_detail_id
-    JOIN users u ON od.employee_code = u.user_id
-    WHERE od.responeCode IS NULL AND u.role = 3
-    GROUP BY u.user_id) AS BANG_A
-    JOIN
-    (SELECT debt_history.user_id, SUM(debt_history.price) as sum_nhan
-    FROM clean_house.debt_history
-    GROUP BY user_id) AS BANG_B
-    ON BANG_A.user_id = BANG_B.user_id;`,
+    `SELECT u.*, COALESCE(BANG_A.debt, 0) AS debt, COALESCE(BANG_B.receipt, 0) AS re, (COALESCE(BANG_A.debt, 0) - COALESCE(BANG_B.receipt, 0)) as balance
+    FROM users u
+    LEFT JOIN
+        (SELECT od.employee_code, SUM(od.unit_price) AS debt
+        FROM emoloyee_debt ed
+        JOIN order_details od ON ed.order_detail_id = od.order_detail_id
+        WHERE od.responeCode IS NULL
+        GROUP BY od.employee_code) AS BANG_A
+        ON u.user_id = BANG_A.employee_code
+    LEFT JOIN
+        (SELECT dh.user_id, SUM(dh.price) as receipt
+        FROM debt_history dh
+        GROUP BY dh.user_id) AS BANG_B
+        ON u.user_id = BANG_B.user_id
+    WHERE u.role = 3`,
     [params],
     (error, results) => {
       if (error) {
@@ -93,18 +103,29 @@ const getListEmployeeReceiptId = (params, callback) => {
 
 // receipt employee debt
 const AddEmployeeDebt = (params, callback) => {
-  const date = moment(params.repayment_at).format('YYYY-MM-DD HH:mm:ss');
+  const date = moment(params.repayment_at).format("YYYY-MM-DD HH:mm:ss");
   connection.query(
     `INSERT INTO debt_history (user_id, repayment_at, price, order_detail_id,employee_debt_id) VALUES (?, ?, ?, ?, ?)`,
-    [params.id, date, params.total, params.order_detail_id, params.employee_debt_id],
+    [
+      params.id,
+      date,
+      params.total,
+      params.order_detail_id,
+      params.employee_debt_id,
+    ],
     (error, results) => {
       if (error) {
-        callback({ message: "Something wrong!" ,error }, null);
+        callback({ message: "Something wrong!", error }, null);
       } else {
-        callback(null, { message: "Success!" ,results });
+        callback(null, { message: "Success!", results });
       }
     }
   );
 };
 
-export default { getListDebtHistory, getListEmployeeReceipt, AddEmployeeDebt, getListEmployeeReceiptId };
+export default {
+  getListDebtHistory,
+  getListEmployeeReceipt,
+  AddEmployeeDebt,
+  getListEmployeeReceiptId,
+};
