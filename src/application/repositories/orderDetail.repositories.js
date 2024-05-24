9a635bf2afddca725683d4cbc0ec5930ea6a7806
anhtai13 <tainguyen2322001@gmail.com>
2024-05-24 21:55:53 +0700

    updated format price

--- src/application/repositories/orderDetail.repositories.js
+++ src/application/repositories/orderDetail.repositories.js
@@ -16,12 +16,37 @@ const getListOrderDetail = (params, callback) => {
 const getOrderDetailById = (params, callback) => {
     console.log(params)
     connection.query(
-        `SELECT od.*,o.*,s.*,u.* 
-    FROM order_details 
-    AS od JOIN orders AS o ON od.order_id = o.order_id JOIN services 
-    AS s ON od.service_id = s.service_id JOIN users 
-    AS u ON o.user_id = u.user_id
-    WHERE o.order_id=?`,
+        `SELECT 
+        od.order_detail_id,
+        od.order_id,
+        od.phone_number,
+        od.service_id,
+        od.note,
+        FORMAT(od.unit_price, 'C', 'en-US') AS unit_price_formatted,
+        FORMAT(od.sub_total_price, 'C', 'en-US') AS sub_total_price_formatted,
+        od.address_order,
+        od.area,
+        od.start_time,
+        od.full_name,
+        od.service_name,
+        od.estimated_time,
+        od.employee_code,
+        od.vnp_ResponseCode,
+        od.payment,
+        od.housetype,
+        od.work_date,
+        od.notification,
+        od.status_payment,
+        o.*, 
+        s.*, 
+        u.*
+    FROM 
+        order_details AS od 
+        JOIN orders AS o ON od.order_id = o.order_id 
+        JOIN services AS s ON od.service_id = s.service_id 
+        JOIN users AS u ON o.user_id = u.user_id
+    WHERE 
+        o.order_id = ?`,
         [params], (error, results) => {
             if (error) {
                 callback({ message: "Something wrong!" }, null);
